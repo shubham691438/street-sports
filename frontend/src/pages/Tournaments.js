f775393ac6b6d8ec332ef01bb227cdc79ad9e373
shubham <ss691438@gmail.com>
2023-04-11 23:09:48 +0530

    created intro section of tournaments page

--- frontend/src/pages/Tournaments.js
+++ frontend/src/pages/Tournaments.js
@@ -1,28 +1,88 @@
-import { Box, Button, Stack, Typography } from '@mui/material'
-import React from 'react'
-import EventsCarousel from '../components/EventsCarousel'
-import { Link } from 'react-router-dom'
+import { Box, Button, styled, Typography,Stack } from "@mui/material";
+import { Container } from "@mui/system";
+import React from "react";
+import EventsCarousal from '../components/EventsCarousel'
+import heroImg from "../assets/images/cricket-tournament.png";
+import CustomButton from "../components/CustomButton";
 
-const Tournaments = () => {
+const Hero = () => {
+  const CustomBox = styled(Box)(({ theme }) => ({
+    display: "flex",
+    justifyContent: "center",
+    gap: theme.spacing(5),
+    marginTop: theme.spacing(3),
+    [theme.breakpoints.down("md")]: {
+      flexDirection: "column",
+      alignItems: "center",
+      textAlign: "center",
+    },
+  }));
+
+  const Title = styled(Typography)(({ theme }) => ({
+    fontSize: "64px",
+    color: "#1adaa7",
+    fontWeight: "bold",
+    margin: theme.spacing(4, 0, 4, 0),
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "40px",
+    },
+  }));
+
   return (
-    <Box color='white' sx={{margin:"5% 5%"}}>
-      <Typography fontSize='30px' fontWeight='bold' color='#1adaa7'>Tournaments</Typography>
-      <Stack direction="row"  spacing={3} justifyContent="space-between" flexWrap={{xs:"wrap-reverse", md:'nowrap'}}>
-            <Stack mx='auto'>
-                <Typography sx={{color:"red",mt:"50px"}}>Get in the game and join the excitement at our sports tournament central!"</Typography>
-                
-                <Typography sx={{color:"#1adaa7",fontWeight:"bold",fontFamily:"sans-serif",fontSize:{lg:"3.5rem",sm:"2rem",xs:"1.5rem"}}}>Unleash your inner athlete and compete with the best at our sports tournament hub!</Typography>
-                <Link to='#' style={{color: "inherit",textDecoration:"none"}}>
-                  <Button variant="contained" sx={{mt:"70px"}}>
-                <Typography variant='h5' fontWeight="500">Explore Now</Typography>
-                <Typography variant='h5' fontWeight="500">Organise Tournaments</Typography>
-                </Button></Link>
+    <Box sx={{minHeight: "80vh" }}>
+      <Container>
+        <CustomBox>
+          <Box sx={{ flex: "1" }}>
+            <Typography
+              variant="body2"
+              sx={{
+                fontSize: "18px",
+                color: "white",
+                fontWeight: "500",
+                mt: 10,
+                mb: 4,
+              }}
+            >
+              Discover Your Next Challenge
+            </Typography>
+            <Title variant="h1">
+            Compete, Conquer, and Connect at Our Sports Tournaments
+            </Title>
+            <Typography
+              variant="body2"
+              sx={{ fontSize: "18px", color: "white", my: 4 }}
+            >
+             We provides a dynamic platform for athletes of all levels
+            to come together and compete in a variety of sports tournaments
+            </Typography>
+            <Stack direction='row' spacing={2}>
+            <CustomButton
+              backgroundColor="#3392FF"
+              color="#fff"
+              buttonText="Explore Now"
+              heroBtn={true}
+            />
+            <CustomButton
+              backgroundColor="#3392FF"
+              color="#fff"
+              buttonText="Organise Tournaments"
+              heroBtn={true}
+            />
             </Stack>
-            {/* <Box component='img'  src={HeroImage} width='100%' height="auto" maxWidth={{xs:"400px",md:"600px"}}  alt='hero'/> */}
-        </Stack>
-      <EventsCarousel/>
-    </Box>
-  )
-}
+          </Box>
 
-export default Tournaments
+          <Box sx={{ flex: "1.25" }}>
+            <img
+              src={heroImg}
+              alt="heroImg"
+              style={{ maxWidth: "100%", marginBottom: "2rem" }}
+            />
+          </Box>
+        </CustomBox>
+      </Container>
+      <EventsCarousal/>
+    </Box>
+  );
+};
+
+export default Hero;
